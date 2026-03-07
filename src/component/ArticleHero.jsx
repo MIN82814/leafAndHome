335ed429496ce794bf473b519720ba2f413df843
Min <min82814@gmial.com>
2026-03-07 10:05:02 +0800

    articleHero空白修正

--- src/component/ArticleHero.jsx
+++ src/component/ArticleHero.jsx
@@ -2,14 +2,15 @@ function ArticleHero({ article }) {
   if (!article) return null;
   return (
     <>
-      <header className="hero-base hero-overlay hero-article-detail">
-        <div className="container hero-content text-md-center px-7 px-md-0">
+      <header className="hero-base hero-overlay  hero-article-detail">
+        <div className="container hero-content text-md-center  px-7 px-md-0">
           <h2
             className="fw-bold mb-5 custom-txt-shadow"
-            dangerouslySetInnerHTML={{ __html: article.title }}
+            dangerouslySetInnerHTML={{ __html:article.title}}
           ></h2>
           <p className="fw-bold h5 custom-txt-shadow">
-            作者：{article.author}
+            作者:{article.author}
+            {" "}
             <span className="d-none d-md-inline-block mx-2">|</span>
             <span className="d-block d-md-inline-block">
               發布日期：
@@ -21,5 +22,4 @@ function ArticleHero({ article }) {
     </>
   );
 }
-
 export default ArticleHero;
--- src/component/ArticlesHero.jsx
+++ src/component/ArticlesHero.jsx
@@ -2,11 +2,11 @@ function ArticlesHero({ categories, selectedTag, setSelectedTag }) {
   if (!categories) return null;
   return (
     <>
-      <header className="hero-base hero-overlay  hero-articles articles-hero-blur">
+      <header className="hero-base hero-overlay hero-articles articles-hero-blur">
         <div className="container hero-content text-md-center px-7 px-md-0">
           <h1 className="fw-bold mb-5 custom-txt-shadow">生活日常誌</h1>
           <p className="fw-bold h5 custom-txt-shadow">
-            探索​植物​的​療癒​力量，​給​新手​的​養植​指南
+            探索植物的療癒力量，給新手的養殖指南
           </p>
           <div className="d-flex gap-2 justify-content-md-center mt-5 flex-wrap">
             {categories.map((tag) => (
